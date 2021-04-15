38e98fe92f457b9135953eda9e62aa66ba027c87
Coraff <Coraff.dev@gmail.com>
2021-04-16 01:37:54 +0300

    updated

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# region Imports
-import discord
-from discord.ext import commands
-import os
+from dotenv import load_dotenv
 import configparser
+import os
+from discord.ext import commands
+import discord
+import time
+time.sleep(5)
+# region Imports
 # endregion
-
 # region Variables
+load_dotenv()
 token = os.getenv("TOKEN")
 
 intents = discord.Intents.all()
@@ -35,7 +38,8 @@ if __name__ == '__main__':
 
 @bot.event
 async def on_ready():
-    print("\n\n─────────────────────────────\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("─────────────────────────────\n")
     bot_guilds = len(bot.guilds)
     bot_members = len(set(bot.get_all_members()))
 
@@ -43,6 +47,7 @@ async def on_ready():
     print(f"[ + ] | User ID:            | {bot.user.id}")
     print(f"[ + ] | Guilds:             | {bot_guilds}")
     print(f"[ + ] | Members:            | {bot_guilds}")
+    print(f"[ + ] | Prefix:             | {prefix}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{bot.command_prefix}help"))
     print(f"[ + ] | Discord.py Version: | {discord.__version__}")
     print("\n─────────────────────────────\n\n")
